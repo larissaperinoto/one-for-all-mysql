24a68124e8531a66e11ad00ca6b92f7181a3760f
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-24 19:32:46 -0300

    Requisito 8 - Busca os albuns de Elis Regina.

---
+++ desafio8.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT
+	ar.nome AS artista,
+    ab.titulo AS album
+FROM SpotifyClone.artistas AS ar
+INNER JOIN SpotifyClone.albuns AS ab ON ab.artista_id = ar.artista_id
+GROUP BY artista, album HAVING artista = 'Elis Regina';
