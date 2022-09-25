1104075f5d90f7c2fc18ace655ae28d3e6df9fe8
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-25 17:15:19 -0300

    Requisito 11 - Renomeia músicas

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,13 @@
-SELECT
-    faixa AS nome_musica,
-    CASE
-        WHEN faixa LIKE '%Bard%' THEN REPLACE(faixa, 'Bard', 'QA')
-        WHEN faixa LIKE '%Amar%' THEN REPLACE(faixa, 'Amar', 'Code Review')
-        WHEN faixa LIKE '%Pais%' THEN REPLACE(faixa, 'Pais', 'Pull Requests')
-        WHEN faixa LIKE '%SOUL%' THEN REPLACE(faixa, 'SOUL', 'CODE')
-        WHEN faixa LIKE '%SUPERSTAR%' THEN REPLACE(faixa, 'SUPERSTAR', 'SUPERDEV')
-	ELSE faixa
-    END AS novo_nome
-FROM SpotifyClone.cancoes;
+SELECT * FROM (
+	SELECT
+		faixa AS nome_musica,
+		CASE
+			WHEN faixa LIKE '%Bard%' THEN REPLACE(faixa, 'Bard', 'QA')
+			WHEN faixa LIKE '%Amar%' THEN REPLACE(faixa, 'Amar', 'Code Review')
+			WHEN faixa LIKE '%Pais' THEN REPLACE(faixa, 'Pais', 'Pull Requests')
+			WHEN faixa LIKE '%SOUL' THEN REPLACE(faixa, 'SOUL', 'CODE')
+			WHEN faixa LIKE '%SUPERSTAR' THEN REPLACE(faixa, 'SUPERSTAR', 'SUPERDEV')
+		END AS novo_nome
+	FROM SpotifyClone.cancoes) AS musicas
+WHERE musicas.novo_nome IS NOT NULL
+ORDER BY nome_musica DESC;
